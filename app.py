5b1a5b742a04c669c744d61020f80bb9f4517108
Tonika <tokich6@gmail.com>
2020-10-16 16:45:55 -0400

    added local postgres db and started working on register user functionality

--- app.py
+++ app.py
@@ -1,15 +1,49 @@
 from os import environ
 from flask import Flask, render_template, request, redirect, session, flash
-# , flash, jsonify, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# jsonify, session
 # from flask.ext.session import Session
 # from tempfile import mkdtemp
 # from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
-# from werkzeug.security import check_password_hash, generate_password_hash
+
 
 from helpers import search_location_id, list_properties, get_hotel_details, get_hotel_photos
 
 # Configure application
 app = Flask(__name__)
+
+# DATABASE SETUP AND SCHEMAS START
+ENV = 'dev'
+if ENV == 'dev':
+    app.debug = True
+    # connect to development database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/homeaway'
+else:
+    app.debug = False
+    # connect to production database
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# initialize db
+db = SQLAlchemy(app)
+
+# define schemas
+class Profile(db.Model):
+    __tablename__ = 'profile'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique = True, nullable = False)
+    hash = db.Column(db.Text, nullable=False)
+
+def __init__(self, username, email, hash):
+    self.username = username
+    self.email = email
+    self.hash = hash
+
+# DATABASE SETUP AND SCHEMAS END
 
 # Set the sessions' secret key to some random bytes. 
 app.secret_key = environ.get('SECRET_KEY')
@@ -19,6 +53,7 @@ check_in = ''
 check_out = ''
 adults_room1 = ''
 rooms = ''
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -142,11 +177,61 @@ def login():
         return render_template('login.html')    
 
 @app.route("/register", methods=['GET', 'POST'])
-def register():
-    return render_template('register.html')
+def register_user():
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        username = request.form.get("username")
+        if not username:
+            flash("must provide username", 403)
+            return redirect ('/register')
+
+        # Query database for username
+        # duplicate = db.execute("SELECT * FROM users WHERE username = :username",
+        #                   username=username)
+        # if duplicate:
+        #     flash("This username already exists, please choose a different one", 403)
+        #     return redirect ('/register')
+
+        email = request.form.get("email")
+        if not email:
+            flash('please provide an email address', 403)
+            return redirect('/register')
+
+        password = request.form.get("password")
+        # Ensure password was submitted
+        if not password:
+            flash("must provide password", 403)
+            return redirect ('/register')
+
+        confirmation = request.form.get("confirm-password")
+        # enfure confirmation was submitted and matches the password
+        if not confirmation:
+            flash("must confirm password", 403)
+            return redirect ('/register')
+        elif confirmation != password:
+            flash("confirm password is not a match", 403)
+            return redirect ('/register')
+
+        # hash the password
+        hash = generate_password_hash(password)
+
+        print(username)
+        print(email)
+        print(hash)
+
+        # insert into database
+        # db.execute("INSERT INTO users(username, hash) VALUES(:username, :hash)", username=username, hash=hash);
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
 
 
 
 if __name__ == '__main__':
-    app.debug = True
     app.run()
