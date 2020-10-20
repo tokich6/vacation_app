477c2848381afcb6bea930b18458ed4c16e1c22c
Tonika <tokich6@gmail.com>
2020-10-20 17:52:47 -0400

    changed packing ideas image on home page

--- app.py
+++ app.py
@@ -223,7 +223,7 @@ def login():
 
         flash('You are successfully logged in!')
         # Redirect user to home page
-        return redirect('/index')
+        return redirect('/')
         # return redirect(request.referrer)
 
     # User reached route via GET (as by clicking a link or via redirect)
