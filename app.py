177960665d18728d34d415af35f8fddb9f6b56d5
Tonika <tokich6@gmail.com>
2020-11-05 16:15:50 -0500

    construct booking db model

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from helpers import search_location_id, list_properties, get_hotel_details, get_
 # Configure application
 app = Flask(__name__)
 
-# Set the sessions' secret key to some random bytes. 
+# Set the sessions' secret key to some random bytes.
 app.secret_key = environ.get('SECRET_KEY')
 
 # define global variables
@@ -40,11 +40,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # initialize db
 db = SQLAlchemy(app)
 
+# import models needs to be after db initiliazation
+# from models import Profile, Booking
+
 class Profile(db.Model):
     __tablename__ = 'profile'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     username = db.Column(db.String(100), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique = True, nullable = False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
     hash = db.Column(db.Text, nullable=False)
 
     def __init__(self, username, email, hash):
@@ -52,7 +55,44 @@ class Profile(db.Model):
         self.email = email
         self.hash = hash
 
+class Booking(db.Model):
+    __tablename__ = 'booking'
+     # random booking id 
+    booking_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    # foreign key
+    user_id = db.Column(db.Integer, db.ForeignKey('profile.id'), nullable=False)
+    hotel_id = db.Column(db.Integer)
+    hotel_name = db.Column(db.Text())
+    check_in = db.Column(db.Date)
+    check_out = db.Column(db.Date)
+    adults_room1 = db.Column(db.Integer)
+    rooms = db.Column(db.Integer)
+    room_name = db.Column(db.Text())
+    total_pay = db.Column(db.Numeric)
+    free_cancellation = db.Column(db.Boolean)
+    cancel_before = db.Column(db.Date)
+    booked_on = db.Column(db.Date, default=db.func.now())
+
+    def __init__(self, booking_id, user_id, hotel_id, hotel_name, check_in, check_out, adults_room1, rooms, room_name, total_pay, free_cancellation, cancel_before, booked_on):
+        self.user_id = user_id
+        self.booking_id = booking_id
+        self.hotel_id = hotel_id
+        self.hotel_name = hotel_name
+        self.check_in = check_in
+        self.check_out = check_out
+        self.adults_room1 = adults_room1
+        self.rooms = rooms
+        self.room_name = room_name
+        self.total_pay = total_pay
+        self.free_cancellation = free_cancellation
+        self.cancel_before = cancel_before
+        self.booked_on = booked_on
+
+
+# create database tables from models
+db.create_all()
 # DATABASE SETUP END
+
 
 # ROUTES START HERE
 @app.route("/", methods=['GET', 'POST'])
@@ -62,10 +102,11 @@ def index():
     day_after = date.today() + timedelta(days=2)
     return render_template('index.html', tomorrow=tomorrow, day_after=day_after)
 
+
 @app.route("/hotels", methods=['GET', 'POST'])
 def list_hotels():
     if request.method == 'GET':
-            return redirect("/")
+        return redirect("/")
     else:
         # extract parameters from the search form
         destination = request.form.get('destination')
@@ -86,7 +127,6 @@ def list_hotels():
         # search_location_id defined in helpers.py
         destination_id = search_location_id(destination)
         print(f'destination id is { destination_id  }')
-        
 
         if not destination:
             flash('Please type in a destination', 'error')
@@ -96,7 +136,8 @@ def list_hotels():
             return redirect("/")
         else:
             # list_properties defined in helpers.py
-            output = list_properties(destination_id, check_in, check_out, adults_room1)
+            output = list_properties(
+                destination_id, check_in, check_out, adults_room1)
             if output == None:
                 return render_template('400.html')
             header = output['header']
@@ -105,6 +146,7 @@ def list_hotels():
             hotels = output['hotels']
             # print(hotels[0])
             return render_template('hotels.html', header=header, totalCount=totalCount, hotels=hotels)
+
 
 @app.route("/hotels/details", methods=['GET', 'POST'])
 def show_hotel_details():
@@ -115,9 +157,10 @@ def show_hotel_details():
     print(check_in)
     print(check_out)
     print(adults_room1)
-    
+
     # get_hotel_details function defined in helpers
-    output_hotel_details = get_hotel_details(hotel_id,check_in, check_out, adults_room1)
+    output_hotel_details = get_hotel_details(
+        hotel_id, check_in, check_out, adults_room1)
     # save necessary output results as variables to access in template
     if output_hotel_details == None:
         return render_template('500.html')
@@ -131,7 +174,7 @@ def show_hotel_details():
     hotel_price = output_hotel_details['property_description']['featuredPrice']['currentPrice']['formatted']
     hotel_rooms = output_hotel_details['property_description']['roomTypeNames']
 
-    # get_hotel_photos defined in helpers 
+    # get_hotel_photos defined in helpers
     output_hotel_photos = get_hotel_photos(hotel_id)
     hotel_images_list = output_hotel_photos['hotel_images']
     # loop through images, format size & add image urls to list
@@ -140,59 +183,69 @@ def show_hotel_details():
         size = image['sizes'][0]['suffix']
         image_url = image['baseUrl'].format(size=size)
         images_url_list.append(image_url)
-    
 
     return render_template('hotel_details.html', hotelID=hotel_id, hotel_name=hotel_name, hotel_stars=hotel_stars, hotel_address=hotel_address,
-     hotel_price=hotel_price,amenities=amenities, what_is_around=what_is_around, tagline=tagline, freebies=freebies, hotel_rooms=hotel_rooms, images_url_list=images_url_list)
-    
+                           hotel_price=hotel_price, amenities=amenities, what_is_around=what_is_around, tagline=tagline, freebies=freebies, hotel_rooms=hotel_rooms, images_url_list=images_url_list)
+
+
 @app.route("/booking", methods=['GET', 'POST'])
 @login_required
 def confirm_booking():
     if request.method == 'POST':
-        
+
         hotel_id = request.form.get('hotel_id')
         hotel_price = request.form.get('hotel_price')
 
         # contact API to check details as well as room rates before final booking confirmation
-        output_hotel_details = get_hotel_details(hotel_id, check_in, check_out, adults_room1)
+        output_hotel_details = get_hotel_details(
+            hotel_id, check_in, check_out, adults_room1)
         if output_hotel_details == None:
             return render_template('500.html')
         hotel_name = output_hotel_details['property_description']['name']
         hotel_stars = output_hotel_details['property_description']['starRating']
         hotel_address = output_hotel_details['property_description']['address']['fullAddress']
-        best_room = output_hotel_details['first_room'] 
+        best_room = output_hotel_details['first_room']
         # get_days defined in helpers.py
         stay_duration = get_days(check_in, check_out)
-    
+
         return render_template('confirm_booking.html', check_in=check_in, check_out=check_out, hotel_rooms=rooms, adults_room1=adults_room1,
-        hotel_id=hotel_id, hotel_name=hotel_name, hotel_stars=hotel_stars, hotel_address=hotel_address, hotel_price=hotel_price, room=best_room,
-        stay_duration=stay_duration)
+                               hotel_id=hotel_id, hotel_name=hotel_name, hotel_stars=hotel_stars, hotel_address=hotel_address, hotel_price=hotel_price, room=best_room,
+                               stay_duration=stay_duration)
 
     # else request is GET
     return redirect('/')
 
+
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def your_bookings():
-    hotel_id = request.form.get('hotel_id')
-    print(hotel_id)
-    hotel_name = request.form.get('hotel_name')
-    print(hotel_name)
-    room_name = request.form.get('room_name')
-    print(room_name)
-    total_pay = request.form.get('total_pay')
-    print(total_pay)
-    free_cancellation = request.form.get('free_cancellation')
-    print(free_cancellation)
+    today = date.today()
 
-    # enter data into database 
-    # data = Booking(hotel_id, hotel_name, check_in, check_out, rooms, adults_room1, 
-    # room_name, total_pay, cancel_before)
-    # db.session.add(data)
-    # db.session.commit()
+    if request.method == 'POST':
+        hotel_id = request.form.get('hotel_id')
+        print(hotel_id)
+        hotel_name = request.form.get('hotel_name')
+        print(hotel_name)
+        room_name = request.form.get('room_name')
+        print(room_name)
+        total_pay = request.form.get('total_pay')
+        print(total_pay)
+        free_cancellation = request.form.get('free_cancellation')
+        print(free_cancellation)
+        cancel_before = request.form.get('cancel_before')
+        print(cancel_before)
 
-    return render_template('your_bookings.html', name=session['username'], hotel_id=hotel_id, hotel_name=hotel_name,
-    room_name=room_name, total_pay=total_pay, free_cancellation=free_cancellation)
+        # enter booking into the database
+        data = Booking(hotel_id, hotel_name, check_in, check_out, adults_room1, rooms, room_name, total_pay, free_cancellation, cancel_before, booked_on=today)
+        db.session.add(data)
+        db.session.commit()
+        flash('Your booking is confirmed', 'success')
+        return render_template('your_bookings.html', name=session['username'], today=today, hotel_id=hotel_id, hotel_name=hotel_name,
+                           room_name=room_name, total_pay=total_pay, free_cancellation=free_cancellation)
+    else:
+        return render_template('your_bookings.html', name=session['username'], hotel_id=hotel_id, hotel_name=hotel_name,
+                           room_name=room_name, total_pay=total_pay, free_cancellation=free_cancellation)
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register_user():
@@ -201,39 +254,40 @@ def register_user():
         username = request.form.get("username")
         if not username:
             flash('Please provide a username', 'error')
-            return redirect ('/register')
+            return redirect('/register')
         # Query database for username
         elif db.session.query(Profile).filter(Profile.username == username).count() != 0:
             flash('The username already exists, please log in if previously registered or choose a different one', 'error')
-            return redirect ('/register')
+            return redirect('/register')
 
         email = request.form.get("email")
         if not email:
             flash('please provide an email address', 'error')
             return redirect('/register')
         elif db.session.query(Profile).filter(Profile.email == email).count() != 0:
-            flash('The email address you entered is already associated with an account, please log in', 'error')
-            return redirect ('/login')    
+            flash(
+                'The email address you entered is already associated with an account, please log in', 'error')
+            return redirect('/login')
 
         password = request.form.get("password")
         # Ensure password was submitted
         if not password:
             flash('must provide password', 'error')
-            return redirect ('/register')
+            return redirect('/register')
 
         confirm_password = request.form.get("confirm-password")
         # enfure confirmation was submitted and matches the password
         if not confirm_password:
             flash('must confirm password', 'error')
-            return redirect ('/register')
+            return redirect('/register')
         elif confirm_password != password:
             flash('confirm password is not a match', 'error')
-            return redirect ('/register')
+            return redirect('/register')
 
         # hash the password
         hash = generate_password_hash(password)
 
-        # enter data into database 
+        # enter data into database
         data = Profile(username, email, hash)
         db.session.add(data)
         db.session.commit()
@@ -244,6 +298,7 @@ def register_user():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("register.html")
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -256,20 +311,21 @@ def login():
         # Ensure username was submitted
         if not request.form.get("username"):
             flash('must provide username', 'error')
-            return render_template ('login.html')
+            return render_template('login.html')
 
         # Ensure password was submitted
         elif not request.form.get("password"):
             flash('must provide password', 'error')
-            return render_template ('login.html')
+            return render_template('login.html')
 
         # query database for the username input
-        user = db.session.query(Profile).filter(Profile.username == request.form.get('username')).first()
+        user = db.session.query(Profile).filter(
+            Profile.username == request.form.get('username')).first()
 
         # Ensure username exists and password is correct
         if not user or not check_password_hash(user.hash, request.form.get("password")):
             flash('invalid username and/or password, please try again', 'error')
-            return render_template ('login.html')
+            return render_template('login.html')
 
         # Remember which user has logged in
         session["user_id"] = user.id
@@ -283,7 +339,8 @@ def login():
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template('login.html')    
+        return render_template('login.html')
+
 
 @app.route("/logout")
 def logout():
